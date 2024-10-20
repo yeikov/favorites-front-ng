1795a90c33f3a94ab294d449d8a04c24acfd6745
yeikov <jacobo.m.f@gmail.com>
2024-10-20 11:21:59 +0200

    adds registry description in assessments list

--- src/app/components/assessment-list/assessment-list.component.ts
+++ src/app/components/assessment-list/assessment-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AssessmentService } from '../../services/assessment.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SessionService } from '../../services/session.service';
+import { RegistryService } from '../../services/registry.service';
 
 @Component({
   selector: 'app-assessment-list',
@@ -14,10 +15,12 @@ export class AssessmentListComponent implements OnInit {
   constructor(
     private assessmentService: AssessmentService,
     public sessionService: SessionService,
+    public registryService: RegistryService,
     private router: Router,
     private activatedRoute: ActivatedRoute) { }
 
   paramId = '';
+  registry = {title: '', media: '', author: '', productionDate: ''};
   resIn: boolean = false;
   list: any;
 
@@ -29,6 +32,10 @@ export class AssessmentListComponent implements OnInit {
       this.paramId = params['id'];
 
 
+    })
+
+    this.registryService.one(this.paramId).subscribe( res => {
+      this.registry = res;
     })
 
     this.assessmentService.registry(this.paramId).subscribe(res => {
