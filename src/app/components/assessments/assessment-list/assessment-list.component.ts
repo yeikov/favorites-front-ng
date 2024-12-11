f5262ac1bd54158ebba4e808e1e9d390a0c10f5d
yeikov <jacobo.m.f@gmail.com>
2024-12-11 18:22:36 +0100

    assessments by registry - pagination

--- src/app/components/assessments/assessment-list/assessment-list.component.ts
+++ src/app/components/assessments/assessment-list/assessment-list.component.ts
@@ -5,10 +5,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { RegistryService } from '../../registries/registry.service';
 import { SessionService } from '../../../common/frame/login/session.service';
+import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
+
+import { Page } from '../../../common/utilities';
 
 @Component({
   selector: 'app-assessment-list',
   standalone: true,
+  imports: [NgbPagination],
   templateUrl: './assessment-list.component.html',
   styleUrls: ['./assessment-list.component.css']
 })
@@ -28,20 +32,23 @@ export class AssessmentListComponent implements OnInit {
   favorite: boolean = false;
   recommend: boolean = false;
 
+  number = 0;
+  totalElements=0;
+  totalPages=0;
+  size=10
+  numberOfElements = 0;
+  first=true;
+  last=true;
+  empty=true;
+
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       this.paramId = params['id'];
-
-
     })
 
     this.registryService.one(this.paramId).subscribe( res => {
       this.registry = res;
     })
-
-    this.assessmentService.registry(this.paramId).subscribe(res => {
-      this.list = res;
-    });
 
   }
 
@@ -49,5 +56,23 @@ export class AssessmentListComponent implements OnInit {
     this.router.navigate(['assessment/' + assessment.id]);
   }
 
+  changePage(event: number){
+    this.number = event - 1;
+     this.assessmentService.registry(this.paramId+`?page=${event-1}&size=${this.size}&sort=registeredAt,asc`).subscribe(res => {
+      this.updatePage (res);
+
+    });
+  }
+
+  updatePage(res:Page){
+    this.list = res.content;
+    this.totalElements = res.totalElements;
+    this.totalPages = res.totalPages;
+    this.number = res.number;
+    this.size = res.size;
+    this.numberOfElements = res.numberOfElements;
+    this.first = res.first;
+    this.last = res.last;
+  }
 }
 
